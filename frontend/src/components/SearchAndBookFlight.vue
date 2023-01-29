2aa0a84a54eed8b410d8dd148d4cea4fc7af98a9
jacobtt21 <jacobt1206@gmail.com>
2023-01-28 21:21:45 -0600

    fixed the dropdown

--- frontend/src/components/SearchAndBookFlight.vue
+++ frontend/src/components/SearchAndBookFlight.vue
@@ -11,47 +11,29 @@
         <label for="inlineRadio20" class="w-10 h-10 bg-gray-300 transition duration-200 p-2 rounded-full peer-checked:bg-sky-300 text-xs">Round Trip</label>
       </div>
     </div>
-    <div v-if="picked === 'true'" class="flex mt-8 items-center">
-      <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg aria-hidden="true" class="w-5 h-5 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-        </div>
-        <input name="start" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="MM/DD/YYYY">
+
+    <h1 class="font-bold text-sm mt-6">Travel Dates</h1>
+    <div class="flow-root">  
+      <div class="float-left w-2/4 p-4">
+        <input name="go" type="text" class="top-0 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="MM/DD/YYYY">
+      </div>
+      <div v-if="picked !== 'true'" class="float-right w-2/4 p-4">
+        <input name="back" type="text" class="top-0 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="MM/DD/YYYY">
       </div>
     </div>
-    <div v-else class="flex mt-8 items-center">
-      <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg aria-hidden="true" class="w-5 h-5 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-        </div>
-        <input name="start" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="MM/DD/YYYY">
-      </div>
-      <span class="mx-4 text-gray-500">-</span>
-      <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg aria-hidden="true" class="w-5 h-5 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-        </div>
-        <input name="end" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="MM/DD/YYYY">
-      </div>
-    </div>
-    <div class="flex mt-8 items-center">
-      <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <img src="https://www.svgrepo.com/show/57834/takeoff-the-plane.svg" class="w-5 h-5 text-gray-500 dark:text-gray-400" />
-        </div>
-        <input name="start" type="text" v-model="dAirport" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="New York">
-        <ul v-if="dAirport !== ''" class="">
-          <li v-for="result in dAList" :key="result.code">{{ result.code }}</li>
+
+    <h1 class="font-bold text-sm">Locations</h1>
+    <div class="flow-root">  
+      <div class="float-left w-2/4 p-4">
+        <input name="start" type="text" v-model="dAirport" class="top-0 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="New York">
+        <ul v-if="dAirport !== ''" class="p-2 rounded border-gray-300 overflow-auto">
+          <li v-for="result in dAList" :key="result.code" @click="setDep(result.code)" class="mt-2 w-full">{{ result.city }} ({{ result.code }})</li>
         </ul>
       </div>
-      <span class="mx-4 text-gray-500">&nbsp;</span>
-      <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <img src="https://www.svgrepo.com/download/122269/plane-landing.svg" class="w-5 h-5 text-gray-500 dark:text-gray-400" />
-        </div>
-        <input name="end" type="text" v-model="aAirport" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="London">
-        <ul v-if="aAirport !== ''">
-          <li v-for="result in aAList" :key="result.code">{{ result.code }}</li>
+      <div class="float-right w-2/4 p-4">
+        <input name="start" type="text" v-model="aAirport" class="top-0 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Los Angeles">
+        <ul v-if="aAirport !== ''" class="p-2 rounded border-gray-300 overflow-auto">
+          <li v-for="result in aAList" :key="result.code" @click="setArr(result.code)" class="mt-2 w-full">{{ result.city }} ({{ result.code }})</li>
         </ul>
       </div>
     </div>
@@ -75,6 +57,14 @@
   async function getAirports() {
     const response = await axios.get('https://timley-travel-tamuahck.herokuapp.com/airports/all')
     airports.value = response.data
+  }
+
+  function setArr(arrival:string) {
+    aAirport.value=arrival
+  }
+
+  function setDep(arrival:string) {
+    dAirport.value=arrival
   }
 
   const dAList = computed(() => {
