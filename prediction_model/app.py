a18a120253e41011c47735f4081201bebdd1aa6f
Roman0180 <59584263+Roman0180@users.noreply.github.com>
2023-01-29 10:42:01 -0600

    Added flask requirements and updated model to use less parameters

--- prediction_model/app.py
+++ prediction_model/app.py
@@ -8,10 +8,7 @@ app.config["DEBUG"] = True
 
 # file path to the saved model
 model_filepath = 'prediction_model.sav'
-try:
-    model = pickle.load(open(model_filepath, 'rb'))
-except:
-    sys.exit('Unable to load the model')
+model = pickle.load(open(model_filepath, 'rb'))
 
 
 @app.route('/predict', methods=['GET'])
@@ -19,15 +16,25 @@ def predict():
     try:
         # Get parameters for temperature
         prediction_attributes = np.array([], dtype='float')
-        params = ["OP_CARRIER","OP_CARRIER_FL_NUM","ORIGIN","DEST","CRS_DEP_TIME","DEP_TIME","TAXI_OUT","WHEELS_OFF","WHEELS_ON","TAXI_IN","CRS_ARR_TIME","ARR_TIME","ARR_DELAY","CANCELLED","CANCELLATION_CODE","DIVERTED","CRS_ELAPSED_TIME","ACTUAL_ELAPSED_TIME","AIR_TIME","DISTANCE","CARRIER_DELAY","WEATHER_DELAY","NAS_DELAY","SECURITY_DELAY","LATE_AIRCRAFT_DELAY"]
-        #TODO: Add step for cleaning data passed in
+        params = ["OP_CARRIER","OP_CARRIER_FL_NUM","ORIGIN","DEST","CRS_DEP_TIME","DEP_TIME","TAXI_OUT","WHEELS_OFF","WHEELS_ON","TAXI_IN","CRS_ARR_TIME","ARR_TIME","ARR_DELAY"]
+        str_cols = ["OP_CARRIER", "ORIGIN", "DEST"]
         for param in params: 
-            prediction_attributes = np.append(prediction_attributes, float(request.args.get((param))))
+            if param in str_cols: 
+                str_val = request.args.get(param)
+                # convert the string to float
+                convert_to_float = lambda x: sum(ord(c) for c in x)
+                prediction_attributes = np.append(prediction_attributes, convert_to_float(str_val))
+            else:
+                prediction_attributes = np.append(prediction_attributes, float(request.args.get((param))))
         features = [prediction_attributes]
         prediction = model.predict(features)
-        output = round(prediction[0][0], 2)
-
-        return {'pred_val': output}
+        output = round(prediction[0], 2)
+        if output < 0: 
+            return {'status' : 'early', 'pred_val': output}
+        elif output < 2.9 and output > 0: 
+            return {'status' : 'on time', 'pred_val': output}
+        else:
+            return {'status' : 'delayed', 'pred_val': output}
     except Exception as e:
         print(e)
         return 'Calculation Error', 500
