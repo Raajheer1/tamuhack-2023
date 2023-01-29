18e0d9131ec2a27ded69cf5633ecfe20e844ce04
jacobtt21 <jacobt1206@gmail.com>
2023-01-29 08:37:05 -0600

    wallet done

--- frontend/src/components/Wallet.vue
+++ frontend/src/components/Wallet.vue
@@ -1,6 +1,53 @@
 <template>
   <div class="rounded-lg font-bold text-black px-10 pt-4">
-    <h2 class="text-2xl">Wallet</h2>
+    <h2 class="text-3xl">Your Wallet</h2>
+    <div class="w-full text-center mt-4">
+      <h4 class="text-7xl">75k<span class="text-2xl">&nbsp;miles</span></h4>
+    </div>
+    <div class="w-full mt-6">
+      <h2 class="text-2xl">Payment methods</h2>
+      <div class="bg-slate-100 p-4 mt-2 flow-root">
+        <div class="float-left">
+          <i class="fa-brands fa-cc-apple-pay"></i>&nbsp;&nbsp;Apple Pay
+        </div>
+        <div class="float-right">
+          Connect
+        </div>
+      </div>
+      <div class="bg-slate-100 p-4 flow-root">
+        <div class="float-left">
+          <i class="fa-brands fa-cc-visa"></i>&nbsp;&nbsp;XXXX XXXX XXXX 5286
+        </div>
+        <div class="float-right">
+          Edit
+        </div>
+      </div>
+    </div>
+    <div class="w-full mt-6">
+      <h2 class="text-2xl">Reward Center</h2>
+      <div class="bg-slate-100 p-4 mt-2 flow-root">
+        <div class="float-left">
+          <h3>Oneworld</h3>
+          <label class="text-xs">
+            Mr. Raaj Patel (XXXX2839)
+          </label>
+        </div>
+        <div class="float-right">
+          <i class="fa-solid fa-check"></i>
+        </div>
+      </div>
+      <div class="bg-slate-100 p-4 flow-root">
+        <div class="float-left">
+          <h3>Oneworld</h3>
+          <label class="text-xs">
+            Raaj Patel (XX-XX2-839)
+          </label>
+        </div>
+        <div class="float-right">
+          <i class="fa-solid fa-check"></i>
+        </div>
+      </div>
+    </div>
   </div>
 
 </template>
